cd9d6fcaae07b5c76e978dd52ea748ce4e389471
abhirupdatta <abhidatta@jhu.edu>
2018-10-30 18:31:37 -0400

    Updating pubs

--- assets/js/togglebutton.js
+++ assets/js/togglebutton.js
@@ -1,11 +1,5 @@
 function toggle_visibility(id) {
     var e = document.getElementById(id);
-    if (e.style.display == 'none') e.style.display = 'block'; 
-    else e.style.display = 'none';
-}
-
-function toggle_visibility_wrap(id) {
-    var e = document.getElementById(id);
-    if (e.style.display == 'none') e.style.display = 'flex';
+    if (e.style.display == 'none') e.style.display = 'block';
     else e.style.display = 'none';
 }
