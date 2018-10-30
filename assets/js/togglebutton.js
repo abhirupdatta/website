9849641192097aae0f4696fae7355b6c6fe1c32d
abhirupdatta <abhidatta@jhu.edu>
2018-10-30 18:44:55 -0400

    Updating the toggle button

--- assets/js/togglebutton.js
+++ assets/js/togglebutton.js
@@ -1,5 +1,11 @@
 function toggle_visibility(id) {
     var e = document.getElementById(id);
-    if (e.style.display == 'none') e.style.display = 'block';
+    if (e.style.display == 'none') e.style.display = 'block'; 
+    else e.style.display = 'none';
+}
+
+function toggle_visibility_wrap(id) {
+    var e = document.getElementById(id);
+    if (e.style.display == 'none') e.style.display = 'flex';
     else e.style.display = 'none';
 }
